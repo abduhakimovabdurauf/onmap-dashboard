256d8057f5ca8871eea488ecfbc236b770f13ff6
abduhakimovabdurauf <abduhakimovabdurauf402@gmail.com>
2025-08-28 14:28:58 +0500

    upload

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@ export interface Bank {
   id: number;
   full_title: string; // jadval uchun
   name: string;
-  buying?: number;
-  selling?: number;
+  buying: number | "no data";
+  selling: number | "no data";
   best_buy?: boolean;
   best_sell?: boolean;
 }
